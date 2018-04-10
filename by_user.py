4666afc608ba1d44ba2bb226fe419a4389b8a75c
bbritain1 <beau.britain417@gmail.com>
2018-04-09 21:12:54 -0400

    add files for by user data format

---
+++ by_user.py
@@ -0,0 +1,29 @@
+from mrjob.job import MRJob
+#import sys
+import os
+
+
+#os.chdir("/Users/beaubritain/Desktop/lecture_notes/Distributed/wikiproject")
+
+
+
+# python by_user.py fixed_subset.txt > output.txt
+
+class subset(MRJob):
+    
+    def mapper(self, _, line):
+        article_id = line.split(" ")[1]
+        #article = line.split(" ")[3]
+        #user_id = line.split(" ")[6].split("\x1e")[0]
+        user_id = line.split(" ")[6]
+        user_id = user_id.split("\u001e")[0]
+        user_id = user_id.split("\\u001e")[0]
+        #user_id = line.split(" ")[5]
+        yield((user_id + "    " + article_id), 1)
+
+    def reducer(self, key, values):
+        yield(key, sum(values))
+
+if __name__ == "__main__":
+    subset.run()
+    
---
+++ user_dataframe.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  8 19:47:31 2018
+
+@author: beaubritain
+"""
+import pandas as pd
+import os 
+import re
+
+
+os.chdir("/Users/beaubritain/Desktop/lecture_notes/Distributed/wikiproject")
+
+columns = []
+users = []
+
+with open("output.txt") as f:
+    for line in f:      
+        new_line = line.split("    ")
+        new_user = re.sub(r'^"', '', new_line[0])
+        new_article = re.sub(r'\"(.*)','', new_line[1])
+        users.append(new_user)
+        columns.append(new_article.rstrip())
+        
+        
+#make users unique
+users = list(set(users))
+
+    
+df_ = pd.DataFrame(index = users, columns=columns)
+df_ = df_.fillna(0)
+
+
+with open("output.txt") as f:
+    for line in f:  
+        new_line = line.split()
+        new_user = re.sub(r'^"', '', new_line[0])
+        new_article = re.sub(r'\"(.*)','', new_line[1])
+        df_.loc[new_user, new_article] = new_line[2]
+ 
+
+df_.to_csv('user_vectors.csv', sep='\t', encoding='utf-8')
+
+
+
